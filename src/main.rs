5d746d0fdfed1a47e5af7da4df54ab8ad92d4485
Fredrik Simonsson <simson@thesimson.net>
2019-01-26 10:46:04 +0900

    Initial implementaion

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-use std::io::{self, Read};
 use std::io::BufRead;
-use regex::Regex;
-use std::u8;
-fn main() {
-    
-    let re_txdata = Regex::new(r"SpiTransaction::read\((.*)\)").unwrap(); 
 
-    for x in io::stdin().lock().lines() {
+/// Simple conversion tool to generate a json array with all lines from a CSV (fields separeated by ; )
+/// Headers in first Line
+/// All other lines as an array
+fn main() {
+    let heading = std::io::stdin().lock().lines().next().unwrap().unwrap() ;
+    let mut headers:Vec<String> = Vec::new();
+    for i in heading.split(';'){
+        headers.push(i.to_string());
+    }
+
+    println!("json = [");
+    for x in  std::io::stdin().lock().lines() {
         match x{
                Err(_) => {} //last
                Ok(x) => {
-                    if re_txdata.is_match(&x){
-                        let y = re_txdata.captures(&x).unwrap().get(1).map_or("", |m| m.as_str());
-                        wdata.push(format!("{}",y));
-                        // println!("//deleted {} //::{}::",x,y);
-
-                    }
-    println!("Hello, world!");
+                   let mut i =0;
+                   let mut h = headers.iter();
+                   for item in x.split(';') {
+                       let title = match h.next(){
+                            Some(x) => {x.to_string()},
+                            None => { format!("default{}",i)}
+                       };
+                       println!("  \"{}\":{}",title,item);
+                       i += 1;
+                   }
              }
         }
     }
+    println!("]");
+
 }
